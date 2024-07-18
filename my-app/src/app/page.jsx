484affd0aa35327905aff487d12d2ac61bdb5987
Sandeep Kandrigi <108851203+skandrigi@users.noreply.github.com>
2024-07-18 02:30:41 -0500

    split page for background design

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -5,24 +5,33 @@ import { FaGooglePlay } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <section>
-      <header>
-        <div className="sm:invisible md:visible l:visible xl:visible">
-          hi
-        </div>
-      </header>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-custom">
-        <div className="text-8xl text-white">
+    <div>
+      <nav>
+        <header>
+          <div className="sm:invisible md:visible lg:visible xl:visible text-white underline text-lg mb-2">
+            hi
+          </div>
+        </header>
+      </nav>
+      <section className="grid grid-cols-2">
+      <main className="flex flex-col items-center justify-center min-h-screen bg-gradient-custom text-white">
+        <div className="text-9xl text-white font-bold">
           Radar
         </div>
-        <div className="">
-
+        <div className="text-xl mt-6">
+          Your Campus, Your Events. Based in College Station.
         </div>
-        <div className="flex-row">
+        <div className="flex flex-row items-center justify-center mt-6 mb-6">
           <FaApple/>
           <FaGooglePlay/>
         </div>
       </main>
-    </section>
+      <div className="pattern-zigzag-3d pattern-blue-500 pattern-bg-white 
+        pattern-size-6 pattern-opacity-20 w-full h-full">
+          zzzzzzzzzzzzzzzzzzzzzzzz
+      </div>
+  
+      </section>
+    </div>
   );
 }
